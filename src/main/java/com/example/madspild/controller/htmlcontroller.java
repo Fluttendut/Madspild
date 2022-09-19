d097fdb1dec22895bed7ce0838614e7e3374705c
Fluttendut <ded_19@hotmail.com>
2022-09-19 21:52:46 +0200

    First

---
+++ src/main/java/com/example/madspild/controller/htmlcontroller.java
@@ -0,0 +1,23 @@
+package com.example.madspild.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class htmlcontroller {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+    @GetMapping("/todays_waste_tomorrows_power")
+    public String twtp() {
+        return "twtp";
+    }
+    @GetMapping("/boardgame")
+    public String boardgame() {
+        return "boardgame";
+    }
+
+}
