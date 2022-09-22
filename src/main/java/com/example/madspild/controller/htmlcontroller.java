e2b2dd56f81c07a8e9222e8fd4c32ae20ce465b6
Zouthen <mich5n69@stud.kea.dk>
2022-09-22 14:12:40 +0200

    calculator functional

--- src/main/java/com/example/madspild/controller/htmlcontroller.java
+++ src/main/java/com/example/madspild/controller/htmlcontroller.java
@@ -1,7 +1,13 @@
 package com.example.madspild.controller;
 
+
+import com.example.madspild.model.DTO;
 import org.springframework.stereotype.Controller;
+
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
@@ -19,8 +25,22 @@ public class htmlcontroller {
     public String boardgame() {
         return "boardgame";
     }
-    @GetMapping("/calculator")
+    @GetMapping("/calculate")
     public String calculator() {
+        return "calculator";
+    }
+
+    @PostMapping("/calculate")
+    public String calculate(@ModelAttribute DTO data, Model model){
+        String value_1 = data.getValue_1();
+        double amountOfPower = 0.54944;
+        double amountOfHeat  = 2.2322;
+        double result2 = Double.parseDouble(value_1) * amountOfHeat;
+        double result1 = Double.parseDouble(value_1) * amountOfPower;
+
+        model.addAttribute("result1",result1);
+        model.addAttribute("result2",result2);
+
         return "calculator";
     }
 
---
+++ src/main/java/com/example/madspild/model/DTO.java
@@ -0,0 +1,22 @@
+package com.example.madspild.model;
+
+public class DTO {
+    private String value_1;
+
+
+    public DTO(String value_1) {
+        this.value_1 = value_1;
+
+    }
+
+
+
+    public String getValue_1() {
+        return value_1;
+    }
+
+    public void setValue_1(String value_1) {
+        this.value_1 = value_1;
+    }
+
+}
