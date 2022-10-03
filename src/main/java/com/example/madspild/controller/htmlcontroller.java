8b9083df5fd3aa2625b295cdb5921d661b60e48f
HkHino <mpfugl@hotmail.com>
2022-10-03 09:11:28 +0200

    correcting something something of something to actually useful text

--- src/main/java/com/example/madspild/controller/htmlcontroller.java
+++ src/main/java/com/example/madspild/controller/htmlcontroller.java
@@ -11,35 +11,44 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
-public class htmlcontroller {
+public class htmlcontroller
+{
 
     @GetMapping("/")
-    public String index() {
+    public String index()
+    {
         return "index";
     }
+
     @GetMapping("/todays_waste_tomorrows_power")
-    public String twtp() {
+    public String twtp()
+    {
         return "twtp";
     }
+
     @GetMapping("/boardgame")
-    public String boardgame() {
+    public String boardgame()
+    {
         return "boardgame";
     }
+
     @GetMapping("/calculate")
-    public String calculator() {
+    public String calculator()
+    {
         return "calculator";
     }
 
     @PostMapping("/calculate")
-    public String calculate(@ModelAttribute DTO data, Model model){
+    public String calculate(@ModelAttribute DTO data, Model model)
+    {
         String value_1 = data.getValue_1();
         double amountOfPower = 0.54944;
-        double amountOfHeat  = 2.2322;
+        double amountOfHeat = 2.2322;
         double result1 = Double.parseDouble(value_1) * amountOfHeat;
         double result2 = Double.parseDouble(value_1) * amountOfPower;
 
-        model.addAttribute("result1",result1);
-        model.addAttribute("result2",result2);
+        model.addAttribute("result1", result1);
+        model.addAttribute("result2", result2);
 
         return "calculator";
     }
